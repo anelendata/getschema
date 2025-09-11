03f046183a80ac9564f19aa0a496675b4a4c68aa
Daigo Tanaka <daigo.tanaka@gmail.com>
2025-09-10 17:00:09 -0700

    Modernize the package with project.toml (#19)
    * Converting to project.toml/uv
    
    * update release script
    
    * remove unnecessary main.py

--- setup.py
+++ setup.py
@@ -1,47 +1,5 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-VERSION = "0.2.11"
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name="getschema",
-    version=VERSION,
-    description="Get jsonschema from sample records",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Daigo Tanaka, Anelen Co., LLC",
-    url="https://github.com/anelendata/getschema",
-
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
-
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: POSIX :: Linux",
-
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
-
-    install_requires=[
-        "setuptools>=40.3.0",
-        "jsonpath-ng>=1.5.2",
-        "python-dateutil>=2.8.1",
-        "simplejson==3.*,>=3.13.2",
-        "pyyaml>=5.1",
-    ],
-    entry_points="""
-    [console_scripts]
-    getschema=getschema:main
-    """,
-    packages=["getschema"],
-    package_data={
-        # Use MANIFEST.ini
-    },
-    include_package_data=True
-)
+if __name__ == "__main__":
+    setup()
